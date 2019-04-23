7e0974fd5ab5fef78dd6e82f1dcf1dab2028ac89
PengShaofeng <3274642663@qq.com>
2019-04-23 15:31:55 +0800

    test

---
+++ app/src/main/java/com/example/lenovo/a421_fragmenttest/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.lenovo.a421_fragmenttest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/lenovo/a421_fragmenttest/RightFragment.java
@@ -0,0 +1,20 @@
+package com.example.lenovo.a421_fragmenttest;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class RightFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_right, container, false);
+        return view;
+    }
+
+}
